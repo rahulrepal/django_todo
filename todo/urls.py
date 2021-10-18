59eb691ba668985d620308a8a33ffd7762b9a45e
rahulrepal <rrepal8@gmail.com>
2021-10-18 23:37:36 +0530

    Adds route for accounts in urls

--- todo/urls.py
+++ todo/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from todo.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home,name="home")
+    path('',home,name="home"),
+    path('account/',include('accounts.urls'),name="accounts")
 ]
